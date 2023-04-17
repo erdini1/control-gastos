d38f42879383bb1b979982a928d37537bc7a8cc3
Dylan Erdini <erdini.dylan@gmail.com>
2023-04-16 21:47:42 -0300

    Formateando las fechas

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { formatearFecha } from '../helpers'
 
 const Gasto = ({ gasto }) => {
 
-  const { nombre, cantidad, categoria, id, fecha} = gasto
+  const { nombre, cantidad, categoria, id, fecha } = gasto
 
 
   return (
     <div className='gasto sombra'>
       <div className='contenido-gasto'>
+        {/* IMAGEN */}
         <div className='descripcion-gasto'>
           <p className='categoria'>
             {categoria}
@@ -15,8 +17,14 @@ const Gasto = ({ gasto }) => {
           <p className='nombre-gasto'>
             {nombre}
           </p>
+          <p className='fecha-gasto'>
+            Agregado el: {""}
+            <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
       </div>
+
+      <p className='cantidad-gasto'>${cantidad}</p>
     </div>
   )
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,13 @@ export const generarId = () => {
     const fecha = Date.now().toString(36)
     return random + fecha
 }
+
+export const formatearFecha = fecha => {
+    const fechanueva = new Date(fecha)
+    const opciones = {
+        year: "numeric",
+        month: "long",
+        day: "2-digit"
+    }
+    return fechanueva.toLocaleDateString("es-ES", opciones)
+}
